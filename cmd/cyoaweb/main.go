b3d6c0cb5747faa8d2b652d2338463d2115ec871
Dominik Link <vorbild94@gmail.com>
2020-06-20 12:28:37 +0200

    added HTML and implemented Http handler

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"awesomeProject/cyoa"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	file := flag.String("file", "story.json", "the JSON with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *file)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
-}
+	h := cyoa.NewHandler(story)
+		fmt.Printf("Starting the server on port: %d\n", *port)
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), h))
+	}
