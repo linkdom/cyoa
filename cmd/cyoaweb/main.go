38ea973a1c1beb473666d74a519c87e3a883a051
Dominik Link <vorbild94@gmail.com>
2020-06-20 11:58:30 +0200

    refactor code

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"awesomeProject/cyoa"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -18,10 +17,8 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
 
